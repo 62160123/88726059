ac358f6387717b5e71fa9a4c3890f0cadfbed443
62160123 <62160123@go.buu.ac.th>
2021-02-13 22:47:02 +0700

    data advance search

---
+++ hw7/postsearchh.php
@@ -0,0 +1,42 @@
+<?php
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_password = "";
+    $db_name = "hw6_62160123";
+
+    $mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
+    $mysqli->set_charset("utf8"); //รองรับภาษาไทย 
+
+    // check connection error 
+    if ($mysqli->connect_errno) {
+        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    } else {
+        // connect success, do nothing
+        //echo "Connect Success";
+    }
+
+    //$kw = $_GET['kw'];
+    $kw = $_POST['kw'];
+    $typ = $_POST['typ']; //postsearch2
+
+    if($kw == ""){
+    }else{
+        $sql = "SELECT musicName, musicLenght, albumName, artistName, albumYear
+                FROM music
+                WHERE musicName LIKE '%$kw%' AND albumName LIKE '%$typ%'";
+        $result = $mysqli->query($sql);
+    }
+
+
+    $arr = array();
+    if($result->num_rows > 0){
+        // Convert MySQL Result Set to PHP Array of object
+        while($row = $result->fetch_object())
+        {
+            $arr[] = $row;
+        }
+    }else {
+      //  echo "Not found";
+    }
+
+    echo json_encode($arr);
